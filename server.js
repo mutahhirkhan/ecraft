1c9c847d10a814b6765e78128346a7b8a0c30979
mutahhirkhan <mutahhir199@gmail.com>
2021-06-05 12:57:40 +0500

    mongodb atlas connected

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const express = require("express")
+const mongoose = require('mongoose');
+const mongoDB_password = "vLOGDlXoYoZ6PsVW"
+mongoose.connect('mongodb+srv://Mutahhir:vLOGDlXoYoZ6PsVW@cluster0.slryu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true}).then((con) => {
+    console.log('connected')
+    console.log(con.connections)
+});
+
 const app = express()
+
+
 
 app.listen(8000, () => {
     console.log('server running on port 8000')
