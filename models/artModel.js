b8442f02cc1d0a900029c57c2d69a503376e01a1
mutahhirkhan <mutahhir199@gmail.com>
2021-06-06 23:04:44 +0500

    validation comments added

--- models/artModel.js
+++ models/artModel.js
@@ -1,6 +1,15 @@
 const mongoose = require("mongoose")
 
 //ye collection ka Data Structure he 
+//is collection me in fields k elawa koi aur extra field inject ni ho sakti
+// lakin kam ho sakti hen fields, but is se ziyada nhi 
+// agar required ki validation lgani he tw is trah 
+/*
+ttile : {
+    type: String,
+    required: true
+}
+*/
 const artSchema = new mongoose.Schema({
     title: String,
     rating: Number,
