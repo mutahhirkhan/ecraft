98a5eae2f4523dd3a32210376533004a936d805f
mutahhirkhan <mutahhir199@gmail.com>
2021-07-13 00:04:51 +0500

    reset password 2/2 done

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,12 +2,27 @@ const User = require("../models/userModal");
 const JWT = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const { promisify } = require("util");
+const sendEmail = require("../models/utility/email");
+const crypto = require("crypto");
 
 var signJWT = (userId) => {
   return JWT.sign({ id: userId }, process.env.JWT_WEB_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
 };
+
+const createAndSendToken =(user, res) => {
+  var token = signJWT(user._id)
+  var { password, ...modifiedUser } = user.toObject(); // converted to simple object
+
+  res.status(200).json({
+    token,
+    status:"success",
+    data:{
+      user: modifiedUser,
+    }
+  })
+}
 
 //for admin
 exports.fetchUsers = async (req, res) => {
@@ -29,19 +44,10 @@ exports.fetchUsers = async (req, res) => {
 exports.signup = async (req, res) => {
   try {
     var user = await User.create(req.body); //bson
+
     //removing password
-    var { password, ...modifiedUser } = user.toObject(); // converted to simple object
-    console.log(modifiedUser);
     //generate JWT
-    var token = signJWT(modifiedUser._id);
-    console.log(token);
-    res.status(200).json({
-      status: "success",
-      token,
-      data: {
-        user: modifiedUser,
-      },
-    });
+    createAndSendToken(user, res)
   } catch (error) {
     res.status(404).json({
       error: error.message,
@@ -71,17 +77,8 @@ exports.login = async (req, res) => {
         error: "invalid email or password", //tell that both things has to verify, for security purpose
       });
     //generate token
-    var token = signJWT(user._id);
-
     //send response
-    var { password, ...modifiedUser } = user.toObject();
-    res.status(200).json({
-      status: "success",
-      token,
-      data: {
-        user: modifiedUser,
-      },
-    });
+    createAndSendToken(user, res)
   } catch (error) {
     res.status(404).json({
       error: error.message,
@@ -138,15 +135,97 @@ exports.protected = async (req, res, next) => {
 //agar parameters bhejny hen function me, tw usko wrap karke bhejo
 //this function will check if user has access to perform the action
 //check artRoutes for more.
-exports.restrictTo = (...roles) =>  (req, res, next) => {
-  console.log(roles)
-  console.log(req.user.role)
-  if(!roles.includes(req.user.role)) {
-    return res
-    .status(401)
-    .json({ error: "you don't have access to perform this action" });
+exports.restrictTo =
+  (...roles) =>
+  (req, res, next) => {
+    console.log(roles);
+    console.log(req.user.role);
+    if (!roles.includes(req.user.role)) {
+      return res
+        .status(401)
+        .json({ error: "you don't have access to perform this action" });
+    }
+    next();
+  };
+
+exports.forgotPassword = async (req, res) => {
+  try {
+    //1- fetch user on the basis of email
+    var { email } = req.body;
+
+    var user = await User.findOne({ email });
+    console.log("user: ", user);
+    if (!user) {
+      return res.status(401).json({
+        status: "error",
+        data: {
+          msg: "no user found",
+        },
+      });
+    }
+    //2- generate reset token
+    var resetToken = user.passwordResetTokenGenerator();
+    await user.save({ validateBeforeSave: false }); //again saving already existing doc, stpping validations
+    var msg = `- click on the following link to reset your password \n- note that the link will expires in 10 minutes \n- ${process.env.BASE_URL}/api/v1/auth/reset-password/${resetToken}`;
+    //3- send it to user's email
+    await sendEmail({
+      to: email,
+      subject: "password reset token",
+      body: msg,
+    });
+    res.status(200).json({
+      status: "success",
+      data: {
+        msg: "reset token has been sent to the respective email address",
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ error: error.message });
   }
-  next();
-  
-  
+};
+
+exports.resetPassword = async (req, res) => {
+  try {
+    var { token } = req.params;
+    var {password, confirmPassword} = req.body
+
+    //encrypt reset token
+    var encryptedResetToken = crypto.createHash("sha256").update(token).digest("hex");
+
+    //get user on the basis of passwordResetToken
+    var user = await User.findOne({
+      passwordResetToken: encryptedResetToken,
+      passwordResetTokenExpiresAt: { $gt: Date.now() },
+    });
+
+    //if user doesn't exists then send error in response
+    if(!user) {
+      return res.status(401).json(  {
+        status: "error",
+        data: {
+          msg: "token doesn't exist or has beene expired",
+        },
+      });
+    }
+    user.password = password,
+    user.confirmPassword = confirmPassword
+    user.passwordResetToken = undefined
+    await user.save()
+    //login user again (signin again/ generating JWT)
+    createAndSendToken(user, res)
+
+    res.status(200).json({
+      data: {
+        msg: "reset Password",
+      },
+    });
+  } catch (error) {
+    user.passwordResetToken =undefined,
+    user.passwordResetTokenExpiresAt =undefined,
+    await user.save({ validateBeforeSave: false }); //again saving already existing doc, stpping validations
+
+    console.log(error);
+    res.status(404).json({ error: error.message });
+  }
 };
